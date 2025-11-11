6884c921620e3ee68ec77a3799b7cb78589996f9
Alice Huang <alicealice.huang@mail.utoronto.ca>
2025-11-11 17:20:56 -0500

    Add fine-grain model, remove flexible model

--- job_script.sh
+++ job_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-aliceh
-#SBATCH --time=2:00:00
-#SBATCH --array=0-8
+#SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=altruism
 #SBATCH --output=%x-%j.out
@@ -9,13 +8,11 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-echo "Starting SLURM task ${SLURM_ARRAY_TASK_ID}..."
+echo "Begin"
 
 module load python/3.13
 module load scipy-stack
 source ~/.virtualenvs/ENV/bin/activate
+python3 model.py
 
-# Pass the array task ID as argument to the script
-python3 model.py ${SLURM_ARRAY_TASK_ID}
-
-echo "Done with task ${SLURM_ARRAY_TASK_ID}"
+echo "Done"
--- model.py
+++ model.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 import ast
-import itertools
-import os
-import sys
+from concurrent.futures import ProcessPoolExecutor
 
 
 # ### First Phase (5 agents, 3 balls fixed)
@@ -24,13 +22,16 @@ class Agent():
         self.urn = np.array([]) # probability [nature, agent 1, agent 2,...]
         self.belief = 0 # current belief
         self.choice = None
-        self.n_success = 0
+        self.n_success = 0 # accumulted number of successes
     def choose(self):
+        """randomly draw one ball"""
         prob = self.urn/sum(self.urn)
         return np.random.choice(len(self.urn), size=1, p=prob)[0]
     def experiment(self):
+        """consult nature, chance of success = reliability"""
         return 1 if random.random() < self.r else 0
     def update(self):
+        """update belief based on choice"""
         self.choice = int(self.choose())
         if self.choice == 0:
             self.belief = self.experiment()
@@ -53,18 +54,19 @@ class Model():
         self.gen_per_run = gen_per_run
         self.agents = [] # list of agents
         self.Rs = [round(random.random(), 2) for i in range(n)] # varying reliability
-        """Create agents"""
+        """create agents"""
         for i in range(n):
             self.agents.append(Agent(self, i, self.Rs[i]))
         self.init_agents()
     def init_agents(self):
+        """update agent parameters"""
         for a in self.agents:
             a.peers = self.agents
             a.urn = np.array([1] * (self.n + 1))
             a.urn[a.id+1] = 0
     def play(self):
         ls = list(range(self.n))
-        random.shuffle(ls)
+        random.shuffle(ls) # Agents update in random order
         for i in ls:
             a = self.agents[i]
             a.update()
@@ -72,16 +74,20 @@ class Model():
     def run(self):
         for i in range(self.gen_per_run):
             for a in self.agents:
-                a.belief = a.experiment()
+                a.belief = a.experiment() # Initiate agent beliefs by consulting nature
             for i in range(self.round_per_gen):
                 self.play()
 
 # #### BH Model (My Version)
 
+# In[63]:
+
+
 class BHAgent(Agent):
     def __init__(self, model, id, reliability, n_votes=3):
         super().__init__(model, id, reliability)
         self.n_votes = n_votes
+        self.results = None
     def choose(self):
         choice = np.array([0]*len(self.urn)) # Array recording balls drawn
         for i in range(self.n_votes):
@@ -92,12 +98,12 @@ class BHAgent(Agent):
         self.urn = self.urn + choice # Return balls back to urn
         return choice
     def update(self):
-        """update belief my majority vote"""
+        """update belief by majority vote"""
         self.choice = self.choose()
-        votes = 0
-        for i in range(self.choice[0]):
-            votes += self.experiment()
-        votes += sum(self.choice[1:] * np.array([a.belief for a in self.model.agents]))
+        ex_results = np.array([sum([self.experiment() for i in range(self.choice[0])])])
+        peer_results = self.choice[1:] * np.array([a.belief for a in self.model.agents])
+        self.results = np.concatenate((ex_results, peer_results), axis=0)
+        votes = sum(self.results)
         if votes > (sum(self.choice) * 0.5):
             self.belief = 1
         elif votes < (sum(self.choice) * 0.5):
@@ -110,7 +116,7 @@ class BHAgent(Agent):
             self.urn = self.urn + self.choice
 
 
-# In[7]:
+# In[64]:
 
 
 class BHModel(Model):
@@ -125,7 +131,7 @@ class BHModel(Model):
 
 # #### Add Group Reward (Consensus) to BH Model
 
-# In[8]:
+# In[24]:
 
 
 class CoopWeightAgent(BHAgent):
@@ -139,7 +145,7 @@ class CoopWeightAgent(BHAgent):
             self.urn = self.urn + self.choice * self.w * s
 
 
-# In[9]:
+# In[25]:
 
 
 class CoopBonusAgent(BHAgent):
@@ -151,10 +157,10 @@ class CoopBonusAgent(BHAgent):
     def reinforce(self, s):
         if self.belief:
             self.urn = self.urn + self.choice
-        self.urn = self.urn + self.choice * self.w * s
+        self.urn = self.urn + self.choice * self.w * s # group reinforcement regardless of belief
 
 
-# In[10]:
+# In[26]:
 
 
 class CoopRewardModel(BHModel):
@@ -182,7 +188,7 @@ class CoopRewardModel(BHModel):
 
 # #### Add Competition to BH Model
 
-# In[11]:
+# In[27]:
 
 
 class CompetitionModel(BHModel):
@@ -208,125 +214,63 @@ class CompetitionModel(BHModel):
             a.reinforce(s)
 
 
-# ### Generalized Model (More agents, flexible number of balls)
+# ### Fine-grained reward
 
-# #### Flexible model with individual reward
-
-# In[128]:
+# In[75]:
 
 
-class flexAgent(Agent):
-    
-    def __init__(self, model, id, reliability, prior=5):
+class fgBHAgent(BHAgent):
+    def __init__(self, model, id, reliability, n_votes=3):
         super().__init__(model, id, reliability)
-        self.prior = prior
-        self.votes = []
-        
-    def choose(self):
-        choice = np.array([0]*len(self.urn)) # Array recording selection
-        for i in range(len(choice)):
-            prob = self.urn[i][0]/sum(self.urn[i]) # Prob proportionate to num of balls
-            if random.random() < prob:
-                choice[i] += 1
-        return choice
-        
-    def update(self):
-        """self.choice records who is consulted"""
-        """self.votes records who is correct out of those consulted"""
-        self.choice = self.choose()
-        self.votes = self.choice * (np.concatenate([np.array([self.experiment()]), 
-                                               np.array([a.belief for a in self.model.agents])]))
-        if sum(self.votes) > (sum(self.choice) * 0.5):
-            self.belief = 1
-        elif sum(self.votes) < (sum(self.choice) * 0.5):
-            self.belief = 0
-        else:
-            self.belief = (random.choice([0, 1]))
-        self.n_success += self.belief
-
+        self.n_votes = n_votes
     def reinforce(self):
-        for i in range(len(self.choice)):
-            if self.choice[i]:
-                if self.votes[i]: # increase prob of consulting again if correct this round
-                    self.urn[i][0] += 1
-                else: # reduce prob of consulting again if incorrect this round
-                    self.urn[i][1] += 1
+        if self.belief:
+            self.urn = self.urn + self.results
+
+# In[76]:
 
 
-# In[129]:
-
-
-class flexModel(Model):
-    def __init__(self, n=5, round_per_gen=100, gen_per_run=200, prior=5):
+class fgBHModel(BHModel):
+    def __init__(self, n=5, round_per_gen=100, gen_per_run=200):
         """n: total number of agents"""
-        self.n = n
-        self.round_per_gen = round_per_gen
-        self.gen_per_run = gen_per_run
-        self.agents = [] # list of agents
-        self.Rs = [round(random.random(), 2) for i in range(n)] # varying reliability
-        self.prior = prior
+        super().__init__(n, round_per_gen, gen_per_run)
+        self.agents = []
         for i in range(n):
-            self.agents.append(flexAgent(self, i, self.Rs[i], prior))
+            self.agents.append(fgBHAgent(self, i, self.Rs[i]))
         self.init_agents()
-    def init_agents(self):
-        for a in self.agents:
-            a.peers = self.agents
-            a.urn = np.full((self.n + 1, 2), a.prior)
-            a.urn[a.id+1] = [0, 1] # agents do not draw themselves
-
-# #### Add group rewards
-
-# In[282]:
 
 
-class flexWeightAgent(flexAgent):
+# In[77]:
+
+
+class fgCoopWeightAgent(CoopWeightAgent):
     "Strength of reinforcement dependent on success of group"
     "Only reinforce if there is individual success"
-    def __init__(self, model, id, reliability, prior=5, w=1):
-        super().__init__(model, id, reliability, prior)
-        self.w = w # weight
+    def __init__(self, model, id, reliability, n_votes=3, w=1):
+        super().__init__(model, id, reliability, n_votes, w)
     def reinforce(self, s):
-        """s: number of (un)successful peers this round"""
-        for i in range(len(self.choice)):
-            if self.choice[i]:
-                if self.votes[i]:
-                    self.urn[i][0] += s * self.w
-                else:
-                    self.urn[i][1] += s * self.w
+        if self.belief:
+            self.urn = self.urn + self.results * self.w * s
 
 
+# In[81]:
 
-class flexCoopModel(flexModel):
-    def __init__(self, n=5, round_per_gen=100, gen_per_run=200, prior=5, w=1):
-        super().__init__(n, round_per_gen, gen_per_run, prior)
-        self.w = w
+
+class fgCoopRewardModel(CoopRewardModel):
+    def __init__(self, n=5, round_per_gen=100, gen_per_run=200, weight=1):
+        super().__init__(n, round_per_gen, gen_per_run, weight)
         self.agents = []
         for i in range(n):
-            self.agents.append(flexWeightAgent(self, i, self.Rs[i], prior, w))
+            self.agents.append(fgCoopWeightAgent(self, i, self.Rs[i]))
         self.init_agents()
-    def play(self):
-        ls = list(range(self.n))
-        random.shuffle(ls)
-        for i in ls:
-            a = self.agents[i]
-            a.update()
-        s = sum([a.belief for a in self.agents]) # Number of successful agents this round
-
-        for a in self.agents:
-            a.reinforce(s)
 
 
-# In[284]:
+# In[82]:
 
 
-class flexCompModel(flexModel):
-    def __init__(self, n=5, round_per_gen=100, gen_per_run=200, prior=5, w=1):
-        super().__init__(n, round_per_gen, gen_per_run, prior)
-        self.w = w
-        self.agents = []
-        for i in range(n):
-            self.agents.append(flexWeightAgent(self, i, self.Rs[i], prior, w))
-        self.init_agents()
+class fgCompetitionModel(fgCoopRewardModel):
+    def __init__(self, n=5, round_per_gen=100, gen_per_run=200, weight=1):
+        super().__init__(n, round_per_gen, gen_per_run, weight)
     def play(self):
         ls = list(range(self.n))
         random.shuffle(ls)
@@ -334,56 +278,52 @@ class flexCompModel(flexModel):
             a = self.agents[i]
             a.update()
         s = self.n - sum([a.belief for a in self.agents]) # Number of unsuccessful agents this round
-        
         for a in self.agents:
             a.reinforce(s)
 
 
-# #### RUN EXPERIMENT
+# #### Test fine-grained reward models
 
-def get_param_combinations():
-    n = [5, 10, 20]
-    prior = [5, 10, 20]
-    return list(itertools.product(n, prior))
+# In[83]:
 
-def run_experiment(index):
 
-    combos = get_param_combinations()
-    if index >= len(combos):
-        raise IndexError(f"Index {index} is out of range (max {len(combos)-1})")
-    
-    n, prior = combos[index]
-
-    columns = ['n', 'prior', 'reliability', 
-           'matrix_flex', 'success_flex',
-           'matrix_flexCoop', 'success_flexCoop',
-           'matrix_flexComp', 'success_flexComp'
+columns = ['reliability', 
+           'matrix_fgBH', 'success_fgBH', 
+           'matrix_fgCoopWeight_F', 'success_fgCoopWeight_F',
+           'matrix_fgCompWeight_F', 'success_fgCompWeight_F'
           ]
-    df = pd.DataFrame(columns=columns)
+df = pd.DataFrame(columns=columns)
+n = 5
 
-    for i in range(100):
-        models = [flexModel(n=n, prior=prior),
-                  flexCoopModel(n=n, prior=prior, w=1),
-                  flexCompModel(n=n, prior=prior, w=1)]
-        R = np.round(np.random.rand(n), 2)
-        data = [n, prior, R]
-        for m in models:
-            m.run()
-            data.append([[round(u[0]/sum(u), 2) for u in a.urn] for a in m.agents])
-            data.append([a.n_success for a in m.agents])
-        df = pd.concat([df, pd.DataFrame([data], columns=columns)], ignore_index=True)
+Rs = pd.read_csv('Rs.csv')
+Rs = Rs['reliability'].apply(ast.literal_eval)
 
-        
 
-    # Save to CSV
-    os.makedirs("results", exist_ok=True)
-    output_file = f"results/result_{index}.csv"
-    df.to_csv(output_file, index=False)
+def run_simulation(i):
+    models = [fgBHModel(n=n),
+              fgCoopRewardModel(n=n, weight=1), 
+              fgCompetitionModel(n=n, weight=1)]
+    
+    for m in models:
+        for a in m.agents:
+            a.r = Rs[i][a.id]
+    
+    data = []
+    data.append(Rs[i])
+    for m in models:
+        m.run()
+        data.append([a.urn for a in m.agents])
+        data.append([a.n_success for a in m.agents])
+    
+    return data
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python experiment_runner.py <job_index>")
-        sys.exit(1)
+# Run in parallel
+with ProcessPoolExecutor() as executor:
+    Results = list(executor.map(run_simulation, range(10)))
 
-    run_experiment(int(sys.argv[1]))
+# Build final DataFrame
+df = pd.DataFrame(Results, columns=columns)
+
+df.to_csv('test.csv', index=False)
+
 
